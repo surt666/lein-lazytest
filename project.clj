70f467cb68e48a4a62e093d7163d2448cc322f79
Thomas Engelschmidt <te@zama.org>
2010-11-21 12:56:00 +0100

    Version 1.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-lazytest "1.0.1-SNAPSHOT"
+(defproject lein-lazytest "1.0.1"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
